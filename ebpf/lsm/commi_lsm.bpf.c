19c3851243d8f99810145f5ede644616d3b685b0
ZhouJin <56241918+FantasySir@users.noreply.github.com>
2025-07-20 11:56:13 +0800

    Implement eBPF hooks and fix build files

--- ebpf/lsm/commi_lsm.bpf.c
+++ ebpf/lsm/commi_lsm.bpf.c
@@ -2,10 +2,29 @@
  * commi_lsm.bpf.c - eBPF LSM hooks for credential checks
  */
 #include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
 
-SEC("lsm")
-int BPF_PROG(commi_task_create, struct task_struct *task) {
-    /* Stub implementation */
+struct task_info {
+    u32 pid;
+    char comm[16];
+};
+
+struct {
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+} task_events SEC(".maps");
+
+SEC("lsm/task_alloc")
+int BPF_PROG(commi_task_create, struct task_struct *task, unsigned long clone_flags)
+{
+    struct task_info event = {};
+
+    event.pid = bpf_get_current_pid_tgid() >> 32;
+    bpf_get_current_comm(&event.comm, sizeof(event.comm));
+
+    bpf_perf_event_output(task, &task_events, BPF_F_CURRENT_CPU,
+                          &event, sizeof(event));
+    bpf_printk("task_alloc pid=%d comm=%s\n", event.pid, event.comm);
+
     return 0;
 }
 
